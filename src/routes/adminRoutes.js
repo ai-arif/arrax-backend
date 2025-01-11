08fa3150138fce5bb5b6c20028352a3ce1caa583
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2025-01-11 18:49:50 +0600

    update

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { getAllUsers, getUserById } = require("../controllers/adminController");
 // get all users,
-router.get("/", getAllUsers);
+router.get("/users", getAllUsers);
 // get single user by id
-router.get("/:id", getUserById);
+router.get("/user/:id", getUserById);
 
 module.exports = router;
